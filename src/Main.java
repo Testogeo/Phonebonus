399215c06b2b87e91352c1cc20ff8986e8ee9d33
MihhFor~ <fr.mikle@ya.ru>
2023-11-27 21:49:30 +0300

    Second modification

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int myschet = 100;     // Объявляете переменные для входных данных и
-        int fullness = 1100; // параметров программы: начального счёта,
-        int edge = 100;               // суммы пополнения и тп
-        int bonus = fullness / edge;
+        int balance = 100;     // Объявляете переменные для входных данных и
+        int topUp = 1100; // параметров программы: начального счёта,
+        int parameter = 100;               // суммы пополнения и тп
+        int bonus = topUp / parameter;
 
-        if (fullness > 1000) {
-            System.out.println("Ваш счет пополнен и добавлен бонус:" + (myschet + fullness + bonus));
+        if (topUp > 1000) {
+            System.out.println("Ваш счет пополнен и добавлен бонус:" + (balance + topUp + bonus));
         } else {
-            System.out.println("Ваш счет пополнен:" + (myschet + fullness));
+            System.out.println("Ваш счет пополнен:" + (balance + topUp));
         }
         // Условным оператором проверяете, превысила ли
         // сумма пополнения порог, и для этих двух разных
